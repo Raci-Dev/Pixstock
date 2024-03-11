4fb11bcecb8a82860a6414bbd7a1e1d2c1e70090
Razy-Cha <raciel.chavez.uicslp.tmz@gmail.com>
2024-03-11 13:46:05 -0600

    Photo page structure & style

--- js/utils/urlDecode.js
+++ js/utils/urlDecode.js
@@ -1,6 +1,16 @@
 /**
-* @copyright codewithsadee 2023
-* @author sadee <codewithsadee@gmail.com>
+* @copyright RCAcademy 2024
+* @author RCA <razymusic4@gmail.com>
 */
 
 "use strict";
+
+
+/**
+ * convert url to object
+ * @param {String} urlString url string
+ * @returns {Object} url object
+ */
+export const urlDecode = urlString => {
+  return Object.fromEntries(urlString.replace(/%23/g, '#').replace(/%20/g, '').split('&').map(i => i.split('=')));
+}
