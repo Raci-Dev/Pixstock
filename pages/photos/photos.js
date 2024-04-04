628858a0ed525a942c0cfab27f9741cc4de87e42
Razy-Cha <raciel.chavez.uicslp.tmz@gmail.com>
2024-04-04 14:05:33 -0600

    Render photos in photo page

--- pages/photos/photos.js
+++ pages/photos/photos.js
@@ -1,6 +1,93 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright RCAcademy
+ * @author RCA <razymusic4@gmail.com>
  */
 
 "use strict";
+
+
+/**
+ * Import
+ */
+import { client } from "../../js/api_configure.js";
+import { gridInit, updateGrid } from "../../js/utils/masonry_grid.js"; 
+import { photoCard } from "../../js/photo_card.js";
+import { updateUrl } from "../../js/utils/updateUrl.js";
+import { urlDecode } from "../../js/utils/urlDecode.js";
+
+
+/**
+ * show filter bar if searched anything
+ */
+const /** {NodeElement} */ $filterBar = document.querySelector('[data-filter-bar]');
+
+$filterBar.style.display = window.location.search ? "flex" : "none";
+
+
+/**
+ * render curated or searched photos
+ * if searched something then render searched photos
+ * otherwise render curated photos
+ */
+const /** {NodeElement} */ $photoGrid = document.querySelector('[data-photo-grid]');
+const /** {NodeElement} */ $title = document.querySelector('[data-title]');
+const /** {Object} */ photoGrid = gridInit($photoGrid);
+const /** {Number} */ perPage = 30;
+let /** {Number} */ currentPage = 1;
+let /** {Number} */ totalPage = 0;
+const /** {String} */ searchUrl = window.location.search.slice(1);
+let /** {Object} */ searchObj = searchUrl && urlDecode(searchUrl);
+const /** {String} */ title = searchObj ? `${searchObj.query} fotos` : "Fotos seleccionadas"
+
+$title.textContent = title;
+document.title = title;
+
+
+/**
+ * render all photos
+ * @param {Number} currentPage Current page number
+ */
+const renderPhotos = function (currentPage) {
+
+  client.photos[searchObj ? 'search' : 'curated']({ ...searchObj, per_page: perPage, page: currentPage }, data => {
+
+    totalPage = Math.ceil(data.total_results / perPage);
+
+    data.photos.forEach(photo => {
+
+      const /** {NodeElement} */ $photoCard = photoCard(photo);
+
+      updateGrid($photoCard, photoGrid.columnsHeight, photoGrid.$columns);
+
+    });
+
+    // when photos loaded
+    isLoaded = true;
+
+    // when no more photo found, hide loader
+    if (currentPage >= totalPage) $loader.style.display = 'none';
+
+  });
+
+}
+
+renderPhotos(currentPage);
+
+
+/**
+ * load more photos
+ */
+const /** {NodeElement} */ $loader = document.querySelector('[data-loader]');
+let /** {Boolean} */ isLoaded = true;
+
+window.addEventListener('scroll', function() {
+
+  if ($loader.getBoundingClientRect().top < (window.innerHeight * 2) && currentPage <= totalPage && isLoaded) {
+
+    currentPage++;
+    renderPhotos(currentPage);
+    isLoaded = false;
+
+  }
+
+});
