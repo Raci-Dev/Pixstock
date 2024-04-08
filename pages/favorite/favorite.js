bdc8198f44ba1936a264c1f69941a856bb4b01f7
Razy-Cha <raciel.chavez.uicslp.tmz@gmail.com>
2024-04-08 17:40:51 -0600

    Render favorite items

--- pages/favorite/favorite.js
+++ pages/favorite/favorite.js
@@ -1,6 +1,61 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright RCAcademy 2024
+ * @author RCA <razymusic4@gmail.com>
  */
 
 "use strict";
+
+
+/**
+ * import
+ */
+import { gridInit, updateGrid } from "../../js/utils/masonry_grid.js";
+import { segment } from "../../js/segment_btn.js";
+import { photoCard } from "../../js/photo_card.js";
+import { videoCard } from "../../js/video_card.js";
+
+
+/**
+ * favorite segment button
+ */
+const /** {NodeElement} */ $favoriteSegment = document.querySelector('[data-segment="favorite"]');
+let /** {String} */ favType = 'photos';
+
+segment($favoriteSegment, segmentValue => {
+  favType = segmentValue;
+
+  $favGrid.innerHTML = '';
+  favGrid = gridInit($favGrid);
+  loadFav(favType, favGrid);
+});
+
+
+/**
+ * load favorite items
+ */
+const /** {NodeElement} */ $favGrid = document.querySelector('[data-fav-grid]');
+let /** {Object} */ favGrid = gridInit($favGrid);
+const favData = JSON.parse(window.localStorage.getItem('favorite'));
+
+const loadFav = function (type, favGridItem) {
+
+  Object.values(favData[type]).forEach(item => {
+
+    let /** {NodeElement} */ $card;
+
+    switch(type) {
+      case 'photos':
+        $card = photoCard(item);
+        break;
+      case 'videos':
+        $card = videoCard(item);
+        break;
+    }
+
+    updateGrid($card, favGridItem.columnsHeight, favGridItem.$columns);
+
+  });
+
+}
+
+loadFav(favType, favGrid);
